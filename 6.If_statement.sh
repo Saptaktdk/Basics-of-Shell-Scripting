777c3868fe29fe19e9e0833e47f0fd03fd6552ea
saptak das <dassaptak0@gmail.com>
2022-09-08 17:57:01 +0530

    added :tada:

---
+++ 6.If_statement.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+# Syntax of if statement
+
+count=10
+
+if [ $count -eq	 9 ]
+then
+	echo "Condition is true"
+else
+	echo "Condition is false"
+fi
+
+
+word=xyz
+
+if [ $word == abc ]
+then
+	echo "True"
+else
+	echo "False"
+fi
