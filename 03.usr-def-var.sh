27e8d3793c42696bd3cae209072f1801f2d861a3
Saptak Das <dassaptak0@gmail.com>
2022-09-09 18:42:46 +0530

    Delete 03.usr-def-var.sh

--- 03.usr-def-var.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-#These are user defined variables
-
-name=Saptak
-echo The name is $name
-
-value=10 #Note: Variable name should not start with a number
-echo The value I have given is $value
